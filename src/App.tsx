9972f06c3fe6eeb20600c87f93f88dd31a285ac0
Tyler Shin <shincode@icloud.com>
2020-07-23 12:22:23 +0900

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,73 @@
-import React, { useRef, useEffect } from "react";
-import isMobile from "ismobilejs";
-import {
-  load,
-  toColoredPartMask,
-  drawPixelatedMask,
-} from "@tensorflow-models/body-pix";
-import { ModelConfig } from "@tensorflow-models/body-pix/dist/body_pix_model";
-import { RAINBOW } from "./constants/colorSet";
-import { drawKeypoints, drawSkeleton, drawAngles } from "./utils/draw";
-
-function getModelConfig(): ModelConfig {
-  if (isMobile(window.navigator).any) {
-    return {
-      architecture: "MobileNetV1",
-      outputStride: 16,
-      multiplier: 0.5,
-      quantBytes: 2,
-    };
-  }
-  return {
-    architecture: "MobileNetV1",
-    outputStride: 16,
-    multiplier: 0.5,
-    quantBytes: 2,
-  };
-}
+import React, { useRef, useEffect, useState } from 'react';
+import { load, toColoredPartMask, drawPixelatedMask } from '@tensorflow-models/body-pix';
+import { RAINBOW } from './constants/colorSet';
+import { drawKeypoints, drawSkeleton, drawAngles } from './utils/draw';
+import s from './App.module.scss';
 
 async function loadModel() {
-  const net = await load(getModelConfig());
+  const net = await load({ architecture: 'MobileNetV1', outputStride: 16, multiplier: 0.5, quantBytes: 2 });
   return net;
 }
 
 function App() {
-  // const imgRef = useRef<HTMLImageElement>(null);
-  const imgRef = useRef<HTMLVideoElement>(null);
+  const [file, setFile] = useState<File>();
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    imgRef.current!.onloadeddata = () => {
-      imgRef.current!.playbackRate = 0.1;
-      loadModel().then((bodyPix) => {
-        async function bodySegmentationFrame() {
-          if (!canvasRef.current) return;
-          const ctx = canvasRef.current.getContext("2d");
-          if (!ctx) return;
+    if (!file || (!file.type.includes('image') && !file.type.includes('video'))) return;
+    if (!canvasRef.current) return;
 
-          const segmentation = await bodyPix.segmentPersonParts(
-            imgRef.current!
-          );
-          const coloredPartImage = toColoredPartMask(segmentation, RAINBOW);
-          const opacity = 0.5;
-          const flipHorizontal = false;
-          const maskBlurAmount = 0;
-          const pixelCellWidth = 5.0;
+    const ctx = canvasRef.current.getContext('2d');
+    const opacity = 0.7;
+    const flipHorizontal = false;
+    const maskBlurAmount = 0;
+    const pixelCellWidth = 5.0;
 
-          drawPixelatedMask(
-            canvasRef.current!,
-            imgRef.current!,
-            coloredPartImage,
-            opacity,
-            maskBlurAmount,
-            flipHorizontal,
-            pixelCellWidth
-          );
+    async function handleImage() {
+      const bodyPix = await loadModel();
+      const url = URL.createObjectURL(file);
+      imageRef.current!.src = url;
 
-          if (segmentation.allPoses[0].keypoints) {
-            drawKeypoints(segmentation.allPoses[0].keypoints, 0.1, ctx);
-            drawSkeleton(segmentation.allPoses[0].keypoints, 0.1, ctx);
-            drawAngles(segmentation.allPoses[0].keypoints, 0.1, ctx);
-          }
+      const segmentation = await bodyPix.segmentPersonParts(imageRef.current!);
+      const coloredPartImage = toColoredPartMask(segmentation, RAINBOW);
 
-          requestAnimationFrame(bodySegmentationFrame);
-        }
+      drawPixelatedMask(
+        canvasRef.current!,
+        imageRef.current!,
+        coloredPartImage,
+        opacity,
+        maskBlurAmount,
+        flipHorizontal,
+        pixelCellWidth
+      );
 
-        bodySegmentationFrame();
-      });
-    };
-  }, []);
+      if (segmentation.allPoses.length > 0 && segmentation.allPoses[0].keypoints) {
+        drawKeypoints(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+        drawSkeleton(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+        drawAngles(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+      }
+    }
+
+    if (file.type.includes('image')) {
+      handleImage();
+    }
+  }, [file]);
 
   return (
-    <div>
-      <h1>DEMO</h1>
-      <canvas ref={canvasRef} id="canvas" />
-      <img
-        // ref={imgRef}
-        src={`${process.env.PUBLIC_URL}/img/demo6.jpg`}
-        alt="demo"
-        style={{ display: "none" }}
-      />
-      <video
-        ref={imgRef}
-        width={800}
-        height={600}
-        autoPlay
-        muted
-        playsInline
-        loop
-        style={{ display: "none" }}
-        src={`${process.env.PUBLIC_URL}/img/demo_video.mp4`}
-      ></video>
+    <div className={s.container}>
+      <h1>Simple road-cycle fitter</h1>
+      <div className={s.fileForm}>
+        <label>
+          <input accept="image/*, video/*" onChange={(e) => setFile(e.currentTarget.files?.[0])} type="file" />
+          Please upload your riding photo or video
+        </label>
+      </div>
+      <div>
+        <canvas ref={canvasRef} id="canvas" />
+      </div>
+      <img ref={imageRef} style={{ display: 'none' }} alt="Riding img" />
     </div>
   );
 }
