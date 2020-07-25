456aec843665db586b8305090ee276caedbb1039
Tyler Shin <shincode@icloud.com>
2020-07-25 16:54:00 +0900

    handle image, video

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { drawKeypoints, drawSkeleton, drawAngles } from './utils/draw';
 import s from './App.module.scss';
 
 async function loadModel() {
-  const net = await load({ architecture: 'MobileNetV1', outputStride: 16, multiplier: 0.5, quantBytes: 2 });
+  const net = await load({ architecture: 'ResNet50', outputStride: 32, quantBytes: 2 });
   return net;
 }
 
@@ -19,6 +19,8 @@ function App() {
     if (!file || (!file.type.includes('image') && !file.type.includes('video'))) return;
     if (!canvasRef.current) return;
 
+    console.log(file.type);
+
     const ctx = canvasRef.current.getContext('2d');
     const opacity = 0.7;
     const flipHorizontal = false;
@@ -30,28 +32,74 @@ function App() {
       const url = URL.createObjectURL(file);
       imageRef.current!.src = url;
 
-      const segmentation = await bodyPix.segmentPersonParts(imageRef.current!);
-      const coloredPartImage = toColoredPartMask(segmentation, RAINBOW);
+      imageRef.current!.onload = async () => {
+        const segmentation = await bodyPix.segmentPersonParts(imageRef.current!);
+        const coloredPartImage = toColoredPartMask(segmentation, RAINBOW);
 
-      drawPixelatedMask(
-        canvasRef.current!,
-        imageRef.current!,
-        coloredPartImage,
-        opacity,
-        maskBlurAmount,
-        flipHorizontal,
-        pixelCellWidth
-      );
+        drawPixelatedMask(
+          canvasRef.current!,
+          imageRef.current!,
+          coloredPartImage,
+          opacity,
+          maskBlurAmount,
+          flipHorizontal,
+          pixelCellWidth
+        );
 
-      if (segmentation.allPoses.length > 0 && segmentation.allPoses[0].keypoints) {
-        drawKeypoints(segmentation.allPoses[0].keypoints, 0.1, ctx!);
-        drawSkeleton(segmentation.allPoses[0].keypoints, 0.1, ctx!);
-        drawAngles(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+        if (segmentation.allPoses.length > 0 && segmentation.allPoses[0].keypoints) {
+          drawKeypoints(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+          drawSkeleton(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+          drawAngles(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+        }
       }
+    }
+
+    async function handleVideo() {
+      console.log("START");
+      const bodyPix = await loadModel();
+      const url = URL.createObjectURL(file);
+      console.log('make url');
+      videoRef.current!.onloadedmetadata = () => {
+        videoRef.current!.width = videoRef.current!.videoWidth;
+        videoRef.current!.height = videoRef.current!.videoHeight;
+      }
+      videoRef.current!.onloadeddata = async () => {
+        console.log("onload!")
+        async function segment() {
+          const segmentation = await bodyPix.segmentPersonParts(videoRef.current!);
+          const coloredPartImage = toColoredPartMask(segmentation, RAINBOW);
+
+          console.log(segmentation);
+
+          drawPixelatedMask(
+            canvasRef.current!,
+            videoRef.current!,
+            coloredPartImage,
+            opacity,
+            maskBlurAmount,
+            flipHorizontal,
+            pixelCellWidth
+          );
+
+          if (segmentation.allPoses.length > 0 && segmentation.allPoses[0].keypoints) {
+            drawKeypoints(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+            drawSkeleton(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+            drawAngles(segmentation.allPoses[0].keypoints, 0.1, ctx!);
+          }
+
+          requestAnimationFrame(segment);
+        }
+
+        segment();
+      }
+
+      videoRef.current!.src = url;
     }
 
     if (file.type.includes('image')) {
       handleImage();
+    } else {
+      handleVideo();
     }
   }, [file]);
 
@@ -68,6 +116,14 @@ function App() {
         <canvas ref={canvasRef} id="canvas" />
       </div>
       <img ref={imageRef} style={{ display: 'none' }} alt="Riding img" />
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        playsInline
+        loop
+        style={{ display: "none" }}
+      />
     </div>
   );
 }
